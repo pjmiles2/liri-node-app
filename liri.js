ad417b7fb1bd618cd3e6521fab41cc9fe3fa70f6
Patrick Miles <patrick.miles@gmail.com>
2018-04-09 23:37:49 -0700

    updated variables to accept multi word titles

--- liri.js
+++ liri.js
@@ -8,17 +8,28 @@ var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 var input = process.argv;
 var command = input[2];
-var title = input[3];
+var inputArray = [];
+
+for (var j=3; j < input.length; j++){
+
+   inputArray.push(input[j]);
+   var title = inputArray.join("+");
+
+}
 
 
+
+console.log(title);
 
 if (command === "my-tweets") {
     myTweets();
-}
+};
 
 if (command === "spotify-this-song") {
     spotifyThisSong(title);
-}
+};
+
+
 
 function myTweets(){
 
@@ -35,21 +46,15 @@ client.get('statuses/user_timeline', params, function(error, tweets, response) {
 
 function spotifyThisSong (title) {
 
- 
-     
     spotify
       .search({ type: 'track', query: "'" + title + "'" })
       .then(function(response) {
         console.log("Artist: " + response.tracks.items[0].artists[0].name);
         console.log("Song Name: " + response.tracks.items[0].name);
-        console.log(
-          "Preview Link of the song: " + response.tracks.items[3].preview_url
-        );
-  console.log("Album Name: " + response.tracks.items[0].album.name);      })
+        console.log("Preview Link on Spotify: " + response.tracks.items[3].preview_url);
+        console.log("Album Name: " + response.tracks.items[0].album.name);      
+    })
       .catch(function(err) {
         console.log(err);
       });
-
-
-
 }
