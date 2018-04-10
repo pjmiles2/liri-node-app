cc349069b685bc44a5a4f145ef2e512fd2682fe6
Patrick Miles <patrick.miles@gmail.com>
2018-04-10 02:04:57 -0700

    cleaned up

--- liri.js
+++ liri.js
@@ -16,7 +16,7 @@ var inputArray = [];
 for (var j=3; j < input.length; j++){
 
    inputArray.push(input[j]);
-   var title = inputArray.join("+");
+   var title = inputArray.join(" ");
 
 }
 
@@ -29,11 +29,7 @@ function doWhatItSays(){
         return console.log(err);
       }
     
-      // Break the string down by comma separation and store the contents into the output array.
       var output = data.split(",");
-      
-      console.log(output);
-
       choices(output[0], output[1]);
     }
     );    
@@ -70,7 +66,7 @@ var params = {PatrickMiles: 'nodejs'};
 client.get('statuses/user_timeline', params, function(error, tweets, response) {
   if (!error) {
       for(var i = 0; i < tweets.length; i ++) {
-    console.log(tweets[i].text);
+    console.log("\uD83D\uDC23 " + tweets[i].text);
   }
 }
 });
@@ -82,10 +78,13 @@ function spotifyThisSong (title) {
     spotify
       .search({ type: 'track', query: title})
       .then(function(response) {
-        console.log("Artist: " + response.tracks.items[0].artists[0].name);
-        console.log("Song Name: " + response.tracks.items[0].name);
-        console.log("Preview Link on Spotify: " + response.tracks.items[3].preview_url);
-        console.log("Album Name: " + response.tracks.items[0].album.name);      
+        console.log("-----------------------------------------------------------------------------")
+        console.log("\u266B Artist: " + response.tracks.items[0].artists[0].name);
+        console.log("\u266B Song Name: " + response.tracks.items[0].name);
+        console.log("\u266B Preview Link on Spotify: " + response.tracks.items[3].preview_url);
+        console.log("\u266B Album Name: " + response.tracks.items[0].album.name);      
+        console.log("-----------------------------------------------------------------------------")
+
     })
       .catch(function(err) {
         console.log(err);
@@ -97,15 +96,16 @@ function movieThis(title) {
     request("http://www.omdbapi.com/?t="+title+"&y=&plot=short&apikey=trilogy", function(error, response, body) {
 
         if (!error && response.statusCode === 200) {
-      
-          console.log("Title: " + JSON.parse(body).Title);
-          console.log("Year: " + JSON.parse(body).Year);
-          console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
-          console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
-          console.log("Country: " + JSON.parse(body).Country);
-          console.log("Language: " + JSON.parse(body).Language);
-          console.log("Plot: " + JSON.parse(body).Plot);
-          console.log("Cast: " + JSON.parse(body).Actors);
+          console.log("-----------------------------------------------------------------------------")            
+          console.log("\uD83C\uDFAC Title: " + JSON.parse(body).Title);
+          console.log("\uD83C\uDFAC Year: " + JSON.parse(body).Year);
+          console.log("\uD83C\uDFAC IMDB Rating: " + JSON.parse(body).imdbRating);
+          console.log("\uD83C\uDFAC Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+          console.log("\uD83C\uDFAC Country: " + JSON.parse(body).Country);
+          console.log("\uD83C\uDFAC Language: " + JSON.parse(body).Language);
+          console.log("\uD83C\uDFAC Plot: " + JSON.parse(body).Plot);
+          console.log("\uD83C\uDFAC Cast: " + JSON.parse(body).Actors);
+          console.log("-----------------------------------------------------------------------------")
 
         }
       });
